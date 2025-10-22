5603e58bf613fc3eba9c4e0921de02ced5674a37
pierrer <pierrer@rvx.is>
2025-10-22 14:42:47 +0000

    fill shots work

--- services/ami/ami/README.md
+++ services/ami/ami/README.md
@@ -1,5 +1,16 @@
 # Todo
 - [x] replace a single cell in an excel sheet with corder
-- [ ] generate an excel file with a range of shots that have been replaced
+- [x] generate an excel file with a range of shots that have been replaced
+- [x] get the fields from the template for shots
+- [ ] make a shot and asset tab and fill them up
 - [ ] make the ami to work on project entity
-- [ ] 
+- [ ] how to download the resulting template from the browser ? 
+
+
+# How it works
+1. read the excel template
+2. get all the fields needed for the shots and assets tabs
+3. make a query of all the shots updated this week with the fields from the template
+4. replace the excel template with corder
+5. post-process: stats on the first page
+6. post-process: replace statuses
--- services/ami/ami/ami_weekly_status_report.py
+++ services/ami/ami/ami_weekly_status_report.py
@@ -1,63 +1,82 @@
-import sys;sys.path.append("/cache/dev/sandbox/rvx/scratches")
+import os
+
+# todo get this lib from somewhere else
 from path_templates import StringTemplate
 import corder
+import ayon_api
+import shotgun_api3
+
+def get_sg_session():
+    ayon_api_key = os.environ.get("AYON_API_KEY")
+    ayon_server_url = os.environ.get("AYON_SERVER_URL")
+    sg_url = os.environ.get("SG_URL")
+    proxy_url = os.environ.get("HTTP_PROXY").replace("http://", "")
+
+    if not ayon_api_key or not ayon_server_url:
+        raise Exception("AYON_API_KEY and AYON_SERVER_URL are required")
+
+    if not sg_url or not proxy_url:
+        raise Exception("SG_URL and HTTP_PROXY env vars are required")
+
+    ayon_api.init_service(token=ayon_api_key, server_url=ayon_server_url)
+    script_name = ayon_api.get_secret("flow_ami_service_name")["value"]
+    script_key = ayon_api.get_secret("flow_ami_service_key")["value"]
+
+    if not script_name or not script_key:
+        raise Exception("Script name or key is not set")
+
+    return shotgun_api3.Shotgun(sg_url, script_name=script_name, api_key=script_key, http_proxy=proxy_url)
 
 
-def test_corder():
-    template = "ami/test_corder.xlsx"
-    out_file = "ami/test_corder_out.xlsx"
-
-    crd = corder.Corder(template)
-    crd.parse_replacements()
-
-    data = {"project": {"name": "Flow_pet_project"}, "root": "dev", "version": {"id": 21342}}
-
-    # set values on tags
-    for tag in crd.tags:
-        stemplate = StringTemplate(tag)
-        value = stemplate.format(data)
-        crd.replacements[tag].set_value(str(value))
-
-    crd.fill()
-    crd.write(out_file)
-
-
-def test_corder_range():
-    # todo this works
-    template = "ami/test_corder_range.xlsx"
-    out_file = "ami/test_corder_range_out.xlsx"
-
-    objects = [
-        {"project": {"name": "Flow_pet_project"}, "root": "dev", "version": {"id": 21342}},
-        {"project": {"name": "Flow_pet_project"}, "root": "toto", "version": {"id": 2324}},
-    ]
+def get_template():
+    template = "ami/template.xlsx"
 
     crd = corder.Corder(template)
     crd.parse_replacements()
 
+    return crd
+
+def get_shot_fields(template: corder.Corder):
+    range = template.range("shot")
+    return [x.lstrip("{").rstrip("}") for x in range.tags]
+
+def get_shots(sg, project_name, fields):
+    return sg.find("Shot", filters=[["project.Project.name", "is", project_name]], fields=fields)
+
+def fill_shots(template, shots):
     rows = []
-    rng = crd.range("version")  # or your range type as defined in the template
-    for obj in objects:
+    rng = template.range("shot")  # or your range type as defined in the template
+    for shot in shots:
+
         row = {}
         for tag in rng.tags:
-            # Render the tag against your object
-            rendered = StringTemplate(tag).format(obj)
+            rendered = StringTemplate(tag).format(shot)
             # If your StringTemplate returns an object with .missing_keys/.invalid_types, handle as needed
-            value = "" if getattr(rendered, "missing_keys", []) or getattr(rendered, "invalid_types", []) else str(
-                rendered)
+            value = "" if getattr(rendered, "missing_keys", []) or getattr(rendered, "invalid_types", []) else str(rendered)
+
             row[tag] = value
         rows.append(row)
 
     rng.set_replacement_values(rows)
-    crd.fill()
-    crd.write(out_file)
+    template.fill()
 
-def test_template():
-    tpl = StringTemplate("/{root}/{project[name]}/asdgasdg")
-    data = {"project": {"name": "Flow_pet_project"}, "root": "dev", "version": {"id": 21342}}
-    print(tpl.format(data))
+
+def export_file(template):
+    out_file = "ami/template_out.xlsx"
+    print(f"Export excel file {out_file}")
+    template.write(out_file)
+
+
+def main():
+    project_name = "Zero_Flow"
+    sg = get_sg_session()
+    template = get_template()
+    shot_fields = get_shot_fields(template)
+    shots = get_shots(sg, project_name, shot_fields)
+    fill_shots(template, shots)
+
+    export_file(template)
+
 
 if __name__ == "__main__":
-    # test_template()
-    # test_corder()
-    test_corder_range()
+    main()
