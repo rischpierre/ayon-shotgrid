ec03db567dc247c1b17771f79d5b991e538b691f
Pierre Risch <rischpierre@gmail.com>
2026-08-09 16:49:23 +0200

    reorganize tests and inherit ami from base

--- services/ami_create_delivery_playlist/ami_create_delivery_playlist/app.py
+++ services/ami_create_delivery_playlist/ami_create_delivery_playlist/app.py
@@ -1,14 +1,15 @@
 import logging
 import os
 import sys
-
-from pydantic import BaseModel, Field, field_validator
-from fastapi import FastAPI, Form, Request
+from datetime import datetime
 from typing import Annotated
+
+from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from jinja2 import ChoiceLoader, FileSystemLoader
+from pydantic import BaseModel, Field, field_validator
 
-from datetime import datetime
+from ami_common import AmiBase
 
 logging.basicConfig(
     level=os.environ.get("LOGLEVEL") or os.environ.get("PYTHON_LOG_LEVEL") or logging.DEBUG,
@@ -49,33 +50,10 @@ class PlaylistSubmitRequest(PlaylistFormRequest):
     name: str
 
 
-class AMICreateDeliveryPlaylist():
+class AMICreateDeliveryPlaylist(AmiBase):
 
     def __init__(self) -> None:
-        if False:
-            self.sg = self.get_sg_session()
-    # todo move this one in the base class
-    def get_sg_session(self):
-        ayon_api_key = os.environ.get("AYON_API_KEY")
-        ayon_server_url = os.environ.get("AYON_SERVER_URL")
-        sg_url = os.environ.get("SG_URL")
-        http_proxy = os.environ.get("HTTP_PROXY", "")
-
-        if not ayon_api_key or not ayon_server_url:
-            raise Exception("AYON_API_KEY and AYON_SERVER_URL are required")
-
-        if not sg_url:
-            raise Exception("SG_URL env var is required")
-
-        ayon_api.init_service(token=ayon_api_key, server_url=ayon_server_url)
-        script_name = ayon_api.get_secret("flow_ami_service_name")["value"]
-        script_key = ayon_api.get_secreet("flow_ami_service_key")["value"]
-
-        if not script_name or not script_key:
-            raise Exception("Script name or key is not set")
-
-        proxy_url = http_proxy.replace("http://", "") if http_proxy else None
-        return Shotgun(sg_url, script_name=script_name, api_key=script_key, http_proxy=proxy_url)
+        super().__init__()
 
     def _generate_playlist_name(self):
         today_str = datetime.now().strftime("%Y-%m-%d")
@@ -115,7 +93,6 @@ class AMICreateDeliveryPlaylist():
         return templates.TemplateResponse(request=request, name="result.html", context=context)
 
         # todo need to process the ids afterwards
-
         selected_ids = request.selected_ids
 
         if not selected_ids:
